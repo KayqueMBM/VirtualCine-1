54cf021d53c84313fc82d56ce5aba1ad03e818f8
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-04-19 07:59:52 -0300

    Virtual Cine

--- combos/combos.js
+++ combos/combos.js
@@ -1,24 +1,25 @@
 function voltar(){
-    window.location.href=("../telaInicial/novaTelaInicial.html");
+   window.open("../telaInicial/novaTelaInicial.html");
 }
 var i = 0
 function carrinho(){
-     
-     var carrinho = document.getElementById('carrinho');
-     
-  
-    i++;
-     
-     if(i > 0) {
-        carrinho.style.position = 'fixed';
-        carrinho.style.width = '30%';
-        carrinho.style.height = '50%';
-        i = 0;
-     } else {
-        carrinho.style.position = 'relative';
-        carrinho.style.width = '70%';
-        carrinho.style.height = '90%';
-        carrinho.classList.add("carrinho");
-     }
-  
+    
+    var carrinho = document.getElementById('carrinho');
+    
+ 
+   i++;
+    
+    if(i === 1) {
+       carrinho.style.position = 'fixed';
+       carrinho.style.width = '30%';
+       carrinho.style.height = '50%';
+       
+    } else {
+       carrinho.style.position = 'relative';
+       carrinho.style.width = '70%';
+       carrinho.style.height = '90%';
+       carrinho.classList.add("carrinho");
+       i = 0;
+    }
+ 
 }
