6392418ff96104646d6abf253f2b3da8322de053
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-05-15 11:17:09 -0300

    Atualizado

---
+++ animacaoTelaInicial/animacaoTelaInicial.js
@@ -0,0 +1,37 @@
+setInterval(function(){
+    $('#v').animate({left:'42%'},2000);
+    $('#v').animate({top:'27%'},500);
+    
+},1000)
+
+setInterval(function(){
+    $('#c').animate({left:'42%'},2000);
+    $('#c').animate({top:'42%'},500);
+   
+
+},1000)
+
+setInterval(function(){
+    $('#macaco').animate({top:'30%'},2500);
+    
+},1000)
+
+setInterval(function(){
+    $('#oculos').animate({top:'32%'},2000);
+    $('#oculos').animate({left:'47%'},500);
+    
+},1000)
+
+setInterval(function(){
+ 
+
+    $('#quad').animate({top:'60%',opacity : '+0.9'},2000);
+    $('#quad').animate({width: '250'},5000);
+    
+},1000)
+
+setInterval(function(){
+
+    $('#quad2').animate({top:'60%',width: '250',opacity : '+0.9'},2000);
+    
+},1000)
