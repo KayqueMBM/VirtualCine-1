a319af8b4cd9b4596cd8fb70ff05d4fe0da27171
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-06-01 11:22:38 -0300

    Atualizado

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -4,6 +4,7 @@
         $sobrenome = $_POST["sobrenome"];
         $email = $_POST["email"];
         $senha = $_POST["senha"];
+        
         $confirmacaoSenha = $_POST["confirmacaoSenha"];
         
         $comando = $pdo->prepare("INSERT INTO usuario(nome,sobrenome,email,senha,confirmacaoSenha) VALUES ('$nome','$sobrenome', '$email','$senha', '$confirmacaoSenha')");
--- cadastro/conecta.php
+++ cadastro/conecta.php
@@ -4,7 +4,7 @@
     
     // CONEXÃO COM A BASE DE DADOS: verifique sempre o dbname
     try{
-        $pdo = new PDO("mysql:dbname=cadastrar-se;host=localhost;charset=utf8","root","");
+        $pdo = new PDO("mysql:dbname=virtualcine;host=localhost;charset=utf8","root","");
     }
     catch(PDOException $erro)
     {
