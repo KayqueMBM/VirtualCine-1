6afa04ffac5fcf61c60ba8a377efd805fb3fa080
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-06-01 11:57:31 -0300

    Atualizado

--- login/login.js
+++ login/login.js
@@ -1,13 +1,13 @@
-  function Login(){
-            var usuario=document.getElementById('usuario').value;
-            usuario=usuario.toLowerCase();
-            var senha=document.getElementById('senha').value;
-            senha=senha.toLowerCase();
-            if (usuario=="kayque" && senha=="123456") {
-                window.open('../telaInicial/novaTelaInicial.html?usuario='+usuario,'Virtual Cine');
-               
-            }
-            else{
-                alert("Dados incorretos, tente novamente");
-            }
-        }
+function Login(){
+    var usuario=document.getElementById('usuario').value;
+    usuario=usuario.toLowerCase();
+    var senha=document.getElementById('senha').value;
+    senha=senha.toLowerCase();
+    if (usuario=="kayque" && senha=="123456") {
+        window.open('../telaInicial/novaTelaInicial.html?usuario='+usuario,'Virtual Cine');
+       
+    }
+    else{
+        alert("Dados incorretos, tente novamente");
+    }
+}
